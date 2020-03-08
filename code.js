a2595054c87d5d5af08368ae5d92f890515f3be3
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2020-03-08 22:59:12 +0200

    delete wrong code

--- code.js
+++ code.js
@@ -55,48 +55,4 @@ document.querySelector('.Menu__buy__buttonBuy').addEventListener('click', functi
   document.querySelector('.Menu__count').innerHTML =  document.querySelector('.Menu__buy__preview').innerHTML
 })
  
-class MyString{
-  // constructor(YourString){
-  //  this.YourString = YourString ;
-  // }
-  reverse(YourString){
-    this.YourString = YourString;
-      
-    //  newString  +=  YourString[YourString.length-1]
-    //  console.log('herestart',YourString[YourString.length-1])
-    //  console.log('herestart', newString )
-    let newString  = '' ;
-    for(   let i = YourString.length    ;  newString.length <= this.YourString.length ;      ){
-      
-      i--
-      newString  +=  YourString[i]
-      
-      console.log(newString.indexOf('undefined') )
-      if(newString.indexOf('undefined') >= 0){
-     let f =  newString.slice(  YourString.length ,  -1);
-     
-      console.log('here f', f)
-    }
-      console.log(`${newString}`)
-    }
-   
-  }
-  usFirst(YourString){
-    this.YourString = YourString;
-     
-return (`${this.YourString[0].toUpperCase()}${this.YourString.slice(1, -1)}`)
-  }
-  usWords(YourString){
-    this.YourString = YourString;
-    return YourString.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
-    // +1).toUpperCase()
- 
-// }
-}
-}
-// const mystring = new MyString('')
- const str = new  MyString();
-console.log(str.usFirst('dasdasa'))
-console.log(str.reverse('1234567890'))
-console.log(str.usWords('hello hi hi hello hi'))
  
